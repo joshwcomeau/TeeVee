ab379c53771f8843306a63763661182f5d1a2d78
Joshua Comeau <joshwcomeau@gmail.com>
2015-11-01 21:25:43 -0500

    BIG change: Added a router, set it up so that the typeahead just forwards to a new path and the router does all the preload stuff. Found a way to render asynchronously, and set up a loader to indicate that things are happening when it's slow.

--- client/main.js
+++ client/main.js
@@ -6,8 +6,11 @@ Episodes  = new Mongo.Collection(null);
 
 Meteor.startup(function () {
   Meteor.typeahead.inject();
-  Meteor.subscribe('seen_episodes');
 });
+
+Template.layout.rendered = function() {
+  Meteor.typeahead.inject();
+};
 
 
 Template.season_list.helpers({
@@ -70,7 +73,7 @@ Template.episode.events({
   }
 });
 
-Template.search.helpers({
+Template.header.helpers({
   // Typeahead handler: Fetches TV show name and IDs from the API.
   retrieveSuggestions: function(query, sync, callback) {
     API.get(TV_MAZE.search(query)).then( (response) => {
@@ -97,63 +100,7 @@ Template.search.helpers({
   
   // Select a show; show all the episodes and their status
   selectShow: function(ev, suggestion, dataset) {
-    // We need to do a few things here:
-    // - retrieve full show details from API
-    // - retrieve episode list from API
-    // - retrieve our `seen` data from Mongo
-    // - set our local Session state variables
-    
-    // TODO: Cache the requested show info so I don't have to re-fetch every time.
-    
-    // Retrieve Show info and episodes
-    let show_info_promise = API.get(TV_MAZE.shows(suggestion.id));
-    let episodes_promise  = API.get(TV_MAZE.episodes(suggestion.id));
-    
-    Promise.all([show_info_promise, episodes_promise]).then( (values) => {
-      let show_info = values[0];
-      let episodes  = values[1];
-      let show_id   = show_info.id;
-      
-      // Insert it into our local (front-end) DB.
-      // Used in the `show_info` template
-      Shows.insert(show_info);
-      
-      // Group episodes by season
-      let seasons = _.values(_.groupBy(episodes, 'season'));
-      
-      seasons.forEach( (episodes_per_season) => {
-        Seasons.insert({
-          showId: show_id,
-          number: episodes_per_season[0].season,
-          length: episodes_per_season.length
-        });
-        
-        episodes_per_season.forEach( (episode) => {
-          // point to its parent show
-          episode.showId = show_id;
-          
-          // Figure out if this user has seen this episode
-          let seen_episode = SeenEpisodes.findOne({
-            userId: Meteor.user()._id,
-            episodeId: episode.id
-          });
-          
-          episode.seen = seen_episode ? seen_episode.seen : false;
-          
-          // Insert it into our local (front-end) DB.
-          // Used in the `season_list` template (and nested templates below)
-          Episodes.insert(episode)
-          
-        });
-      });
-              
-      // Update the session, so we can easily look this show and these episodes
-      // up in the template helpers.
-      Session.set('show_id', show_id);
-      
-    }, (reason) => {
-      console.log("FAILED to fetch TV show info:", reason)
-    });
+    Router.go(`/shows/${suggestion.id}`);
   }
 });
 
